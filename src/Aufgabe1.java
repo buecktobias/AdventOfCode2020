c6cb9f242ad0589a7064784697c08880a7ee5ccd
Tobias Bück <youremail@yourdomain.com>
2020-12-01 18:47:52 +0100

    sumIs works!

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,5 +1,18 @@
 public class Aufgabe1 {
-    public static int add(int a, int b){
-        return a+b;
+    public static int add(int a, int b) {
+        return a + b;
     }
+
+    public static boolean sumIs(int a, int b, int sum){
+        if(add(a,b) == sum){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public static int multiply(int a, int b){
+        return a * b;
+    }
+
 }
--- src/Aufgabe1Test.java
+++ src/Aufgabe1Test.java
@@ -6,6 +6,23 @@ class Aufgabe1Test {
     void add() {
         int a = 5;
         int b = 6;
-        assertTrue(Aufgabe1.add(5,6) == 11);
+        assertTrue(Aufgabe1.add(a,b) == 11);
+        assertTrue(Aufgabe1.add(-2, 3) == 1);
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+        int a = 5;
+        int b = 6;
+        assertTrue(Aufgabe1.multiply(a,b) == 30);
+        assertTrue(Aufgabe1.multiply(-2, 3) == -6);
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumIs() {
+        int a = 5;
+        int b = 6;
+        assertTrue(Aufgabe1.sumIs(a,b, 11));
+        assertFalse(Aufgabe1.sumIs(2,10,3));
     }
 }
