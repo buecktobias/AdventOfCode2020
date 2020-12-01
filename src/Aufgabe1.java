1d8d17a2bfc823f8567f45735eb008a49d114307
Tobias Bück <youremail@yourdomain.com>
2020-12-01 18:37:41 +0100

    created aufgabe 1 and test!!

---
+++ src/Aufgabe1.java
@@ -0,0 +1,5 @@
+public class Aufgabe1 {
+    public static int add(int a, int b){
+        return 0;
+    }
+}
---
+++ src/Aufgabe1Test.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Aufgabe1Test {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+}
