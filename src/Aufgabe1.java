5d390a188826fdef0e290c9643abded852e4c461
Tobias Bück <youremail@yourdomain.com>
2020-12-01 19:23:15 +0100

    hasSum works!

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,18 +1,54 @@
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Aufgabe1 {
     public static int add(int a, int b) {
         return a + b;
     }
 
     public static boolean sumIs(int a, int b, int sum){
-        if(add(a,b) == sum){
-            return true;
-        } else{
-            return false;
-        }
+        return add(a,b) == sum;
     }
 
     public static int multiply(int a, int b){
         return a * b;
     }
+
+    public static boolean isSum2020(int a, int b){
+        return sumIs(a,b,2020);
+    }
+
+    public static boolean hasSum (List<Integer> list){
+        int element1;
+        int element2;
+
+        for(int i = 0; i < list.size()-1; i++) {
+            for(int k = i+1; k < list.size(); k++){
+                element1 = list.get(i);
+                element2 = list.get(k);
+                if(isSum2020(element1, element2)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+/*
+[20,40,6]: length = 3
+i = 0
+k = 1
+20+40 - false
+k = 2
+20+6 -> false
+i = 1
+k = 2
+40+6 -> false
+false
+ */
+
+
+
 
 }
--- src/Aufgabe1Test.java
+++ src/Aufgabe1Test.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Aufgabe1Test {
@@ -25,4 +29,34 @@ class Aufgabe1Test {
         assertTrue(Aufgabe1.sumIs(a,b, 11));
         assertFalse(Aufgabe1.sumIs(2,10,3));
     }
+
+    @org.junit.jupiter.api.Test
+    void isSum2020() {
+        int a = 1000;
+        int b = 1020;
+        assertTrue(Aufgabe1.isSum2020(a,b));
+        assertFalse(Aufgabe1.isSum2020(10, 20));
+    }
+
+    @org.junit.jupiter.api.Test
+    void hasSum() {
+        List<Integer> exampleList1 = Arrays.asList(1, 30, 40, 1000, 1020, 2000);
+        List<Integer> exampleList2 = Arrays.asList(1, 30, 40, 1000, 2000);
+        List<Integer> exampleList3 = Arrays.asList(1, 30, 40, 1000, 1020);
+        List<Integer> exampleList4 = Arrays.asList(1, 3000, 1, 1010, 2000);
+        assertTrue(Aufgabe1.hasSum(exampleList1));
+        assertFalse(Aufgabe1.hasSum(exampleList2));
+        assertTrue(Aufgabe1.hasSum(exampleList3));
+        assertFalse(Aufgabe1.hasSum(exampleList4));
+    }
+
+    @org.junit.jupiter.api.Test
+    void findElements() {
+        List<Integer> exampleList1 = Arrays.asList(1, 30, 40, 1000, 1020, 2000);
+        List<Integer> exampleList2 = Arrays.asList(1, 30, 40, 1000, 2000);
+        assertTrue(Aufgabe1.multiplyElements(exampleList1) == 1000 * 1020);
+        assertTrue(Aufgabe1.multiplyElements(exampleList2) == -1);
+    }
+
+
 }
